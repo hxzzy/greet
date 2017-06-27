7b444554a2303094827ca353705a92452b765f8f
799857634 <799857634@qq.com>
2017-06-27 11:06:01 +0800

    first commit

---
+++ src/GreetServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hxzzy\Greet;
+
+use Illuminate\Support\ServiceProvider;
+
+class GreetServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/greet.php
@@ -0,0 +1,8 @@
+<?php
+namespace Hxzzy\Greet;
+
+class Greet{
+    public static function test(){
+        echo 'hello,world hello,world hello,world hello,world';
+    }
+}
